17f25b29e2f505c8f75b093efda31f6c164d56f2
SojeongKim-42 <k5k5s5s5j5j5@naver.com>
2024-01-12 16:17:25 +0900

    pa3: part1.1 squareToTent() sample warping: 주어진 분포 p를 적분하여 cdf를 만들고, 역함수P^-1(t)를 계산해서 sample을 연산해서 new sample을 만들어 tent distribution으로 재구성한다.

--- src/warp.cpp
+++ src/warp.cpp
@@ -4,22 +4,35 @@
     Copyright (c) 2015 by Wenzel Jakob
 */
 
-#include <nori/warp.h>
-#include <nori/vector.h>
 #include <nori/frame.h>
+#include <nori/vector.h>
+#include <nori/warp.h>
 
 NORI_NAMESPACE_BEGIN
 
-Point2f Warp::squareToUniformSquare(const Point2f &sample) {
-    return sample;
-}
+Point2f Warp::squareToUniformSquare(const Point2f &sample) { return sample; }
 
 float Warp::squareToUniformSquarePdf(const Point2f &sample) {
-    return ((sample.array() >= 0).all() && (sample.array() <= 1).all()) ? 1.0f : 0.0f;
+    return ((sample.array() >= 0).all() && (sample.array() <= 1).all()) ? 1.0f
+                                                                        : 0.0f;
 }
 
 Point2f Warp::squareToTent(const Point2f &sample) {
-    throw NoriException("Warp::squareToTent() is not yet implemented!");
+    float px = 0, py = 0;
+
+    if (sample[0] >= 0 && sample[0] < 0.5f) {
+        px = sqrt(2 * sample[0]) - 1;
+    } else if (sample[0] >= 0.5f && sample[0] <= 1) {
+        px = 1 - sqrt(2 - 2 * sample[0]);
+    }
+
+    if (sample[1] >= 0 && sample[1] < 0.5f) {
+        py = sqrt(2 * sample[1]) - 1;
+    } else if (sample[1] >= 0.5f && sample[1] <= 1) {
+        py = 1 - sqrt(2 - 2 * sample[1]);
+    }
+
+    return Point2f(px, py);
 }
 
 float Warp::squareToTentPdf(const Point2f &p) {
@@ -31,31 +44,38 @@ Point2f Warp::squareToUniformDisk(const Point2f &sample) {
 }
 
 float Warp::squareToUniformDiskPdf(const Point2f &p) {
-    throw NoriException("Warp::squareToUniformDiskPdf() is not yet implemented!");
+    throw NoriException(
+        "Warp::squareToUniformDiskPdf() is not yet implemented!");
 }
 
 Vector3f Warp::squareToUniformSphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformSphere() is not yet implemented!");
+    throw NoriException(
+        "Warp::squareToUniformSphere() is not yet implemented!");
 }
 
 float Warp::squareToUniformSpherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToUniformSpherePdf() is not yet implemented!");
+    throw NoriException(
+        "Warp::squareToUniformSpherePdf() is not yet implemented!");
 }
 
 Vector3f Warp::squareToUniformHemisphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformHemisphere() is not yet implemented!");
+    throw NoriException(
+        "Warp::squareToUniformHemisphere() is not yet implemented!");
 }
 
 float Warp::squareToUniformHemispherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToUniformHemispherePdf() is not yet implemented!");
+    throw NoriException(
+        "Warp::squareToUniformHemispherePdf() is not yet implemented!");
 }
 
 Vector3f Warp::squareToCosineHemisphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToCosineHemisphere() is not yet implemented!");
+    throw NoriException(
+        "Warp::squareToCosineHemisphere() is not yet implemented!");
 }
 
 float Warp::squareToCosineHemispherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToCosineHemispherePdf() is not yet implemented!");
+    throw NoriException(
+        "Warp::squareToCosineHemispherePdf() is not yet implemented!");
 }
 
 Vector3f Warp::squareToBeckmann(const Point2f &sample, float alpha) {
