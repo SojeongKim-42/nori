f018a8f241a983823faaebe26da290f9ea71281e
SojeongKim-42 <k5k5s5s5j5j5@naver.com>
2024-01-17 17:59:23 +0900

    pa3.2: rendering simple point light point light일 때 ray와 mesh의 intersection을 찾고, 그 intersection에서의 빛의 양을 계산한다. L(x)에서 V(x)는 shadow ray 사용.

---
+++ src/simple.cpp
@@ -0,0 +1,41 @@
+#include <nori/integrator.h>
+#include <nori/scene.h>
+
+NORI_NAMESPACE_BEGIN
+
+class SimpleIntegrator : public Integrator {
+   private:
+    Point3f m_position;
+    Color3f m_energy;
+
+   public:
+    SimpleIntegrator(const PropertyList &props) {
+        m_position = props.getPoint("position");
+        m_energy = props.getColor("energy");
+    }
+
+    Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
+        /* Find the surface that is visible in the requested direction */
+        Intersection its;
+        if (!scene->rayIntersect(ray, its)) return Color3f(0.0f);
+
+        Color3f result;
+        Normal3f n = its.shFrame.n;
+        Vector3f xTop = m_position - its.p;
+        float cosTheta = xTop.dot(n) / (xTop.norm() * n.norm());
+        Ray3f shadowRay = Ray3f(its.p, xTop, Epsilon, xTop.norm());
+        int V;
+        scene->rayIntersect(shadowRay) ? V = 0 : V = 1;
+
+        for (int i = 0; i < 3; i++) {
+            result[i] = m_energy[i] / (4 * pow(M_PI, 2)) *
+                        std::max(0.0f, cosTheta) / pow(xTop.norm(), 2) * V;
+        }
+        return result;
+    }
+
+    std::string toString() const { return "SimpleIntegrator[]"; }
+};
+
+NORI_REGISTER_CLASS(SimpleIntegrator, "simple");
+NORI_NAMESPACE_END
